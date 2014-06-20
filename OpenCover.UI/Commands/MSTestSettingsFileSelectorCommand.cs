e0326f7801c5ac0dd4a58c623033b5b922121ee8
jamdagni86 <jamdagni86@gmail.com>
2014-06-21 00:39:15 +0530

    checking in missing file

---
+++ OpenCover.UI/Commands/MSTestSettingsFileSelectorCommand.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Win32;
+//
+// This source code is released under the GPL License; Please read license.md file for more details.
+//
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCover.UI.Commands
+{
+	public class MSTestSettingsFileSelectorCommand : Command
+	{
+		private OpenCoverUIPackage _package;
+
+		internal string SelectedFile {get; private set;}
+
+		public MSTestSettingsFileSelectorCommand(OpenCoverUIPackage package)
+			: base(package, new CommandID(GuidList.GuidOpenCoverUICmdSet, (int)PkgCmdIDList.OpenCoverSelectMSTestSettings))
+		{
+			_package = package;
+			BeforeQueryStatus += OnBeforeQueryStatus;
+		}
+
+		private void OnBeforeQueryStatus(object sender, EventArgs e)
+		{
+			if (SelectedFile != null && File.Exists(SelectedFile))
+			{
+				Text = String.Format("MSTest Settings: {0}", Path.GetFileName(SelectedFile));
+			}
+		}
+
+		protected override void OnExecute()
+		{
+			var dlg = new OpenFileDialog();
+			dlg.Filter = "TestSettings (*.testsettings,*.runsettings,*.vsmdi,*.testrunconfig)|*.testsettings;*.runsettings;*.vsmdi;*.testrunconfig";
+
+			if (dlg.ShowDialog() == true)
+			{
+				SelectedFile = dlg.FileName;
+			}
+		}
+	}
+}
