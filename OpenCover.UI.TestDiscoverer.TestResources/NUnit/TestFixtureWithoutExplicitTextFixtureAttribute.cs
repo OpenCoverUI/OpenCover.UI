937f16b8c81c2ded2dacd4b15b4f92a607337529
Paweł Zając <pavzaj@gmail.com>
2016-02-18 22:36:56 +0100

    Added missing files that broke build

---
+++ OpenCover.UI.TestDiscoverer.TestResources/NUnit/TestFixtureWithoutExplicitTextFixtureAttribute.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+namespace OpenCover.UI.TestDiscoverer.TestResources.NUnit
+{
+    public class TestFixtureWithoutExplicitTestFixtureAttribute
+    {
+        [Test]
+        public void TestMethodInTestFixtureWithoutExplicitTestFixtureAttribute()
+        {
+        }
+    }
+}
