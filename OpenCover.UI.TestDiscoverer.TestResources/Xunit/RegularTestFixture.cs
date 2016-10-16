052ae794746a6a23fd02c414f5f9bf8c4e5a9b6b
MelleKoning <mellekoning@gmail.com>
2016-10-16 22:33:05 +0200

    Update RegularTestFixture.cs
    Fix rename issue

--- OpenCover.UI.TestDiscoverer.TestResources/Xunit/RegularTestFixture.cs
+++ OpenCover.UI.TestDiscoverer.TestResources/Xunit/RegularTestFixture.cs
@@ -1,4 +1,4 @@
-﻿using Xunit;
+using Xunit;
 
 namespace OpenCover.UI.TestDiscoverer.TestResources.Xunit
 {    
@@ -8,5 +8,24 @@ namespace OpenCover.UI.TestDiscoverer.TestResources.Xunit
 		public void RegularTestMethod()
 		{
 		}
-	}
+
+        public class SubTestClass
+        {
+            [Fact]
+            public void RegularSubTestClassMethod()
+            {
+            }
+
+            public class Sub2NdTestClass
+            {
+                [Fact]
+                public void RegularSub2NdTestClassMethod()
+                {
+                }
+            }
+
+
+        }
+
+    }
 }
