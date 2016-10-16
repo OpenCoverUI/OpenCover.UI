2292f9bc8f9b6dcdb58a8cabeaee4bd06720c545
MITEK\mkoning <mkoning@miteksystems.com>
2016-10-16 22:15:12 +0200

    Revert "update"
    This reverts commit 08379b8398bdcc9077e409c87743a6095b4086af.

--- OpenCover.UI.TestDiscoverer.TestResources/xUnit/RegularTestFixture.cs
+++ OpenCover.UI.TestDiscoverer.TestResources/xUnit/RegularTestFixture.cs
@@ -8,5 +8,24 @@ namespace OpenCover.UI.TestDiscoverer.TestResources.Xunit
 		public void RegularTestMethod()
 		{
 		}
-	}
+
+        public class SubTestClass
+        {
+            [Fact]
+            public void RegularSubTestClassMethod()
+            {
+            }
+
+            public class Sub2NdTestClass
+            {
+                [Fact]
+                public void RegularSub2NdTestClassMethod()
+                {
+                }
+            }
+
+
+        }
+
+    }
 }
--- OpenCover.UI.TestDiscoverer.Tests/XUnit/xUnitDiscovererTests.cs
+++ OpenCover.UI.TestDiscoverer.Tests/XUnit/xUnitDiscovererTests.cs
@@ -1,18 +1,27 @@
 ﻿using System;
 using NUnit.Framework;
+using OpenCover.UI.Model.Test;
 using OpenCover.UI.TestDiscoverer.TestResources.Xunit;
 
-//using OpenCover.UI.TestDiscoverer.TestResources.Xunit;
 
 namespace OpenCover.UI.TestDiscoverer.Tests.XUnit
 {
     
-    public class XUnitDiscovererTests: DiscovererTestsBase
+    class XUnitDiscovererTests: DiscovererTestsBase
     {
         [TestCase(typeof(RegularFacts), "RegularTestMethod")]
         public void Discover_Finds_Regular_Test_Fixture_And_Method(Type testFixtureInAssemblyToDiscoverTestsIn, string expectedNameOfFirstTestMethod)
         {
-            AssertDiscoveredMethod(testFixtureInAssemblyToDiscoverTestsIn, expectedNameOfFirstTestMethod);
+            AssertDiscoveredMethod(testFixtureInAssemblyToDiscoverTestsIn, expectedNameOfFirstTestMethod, TestType.XUnit);
         }
+      
+        [TestCase(typeof(RegularFacts.SubTestClass), "RegularSubTestClassMethod")]
+        [TestCase(typeof(RegularFacts.SubTestClass.Sub2NdTestClass), "RegularSub2NdTestClassMethod")]
+        public void Discover_Finds_Sub_Test_Fixtures_And_Methods(Type testFixtureInAssemblyToDiscoverTestsIn, string expectedNameOfFirstTestMethod)
+        {
+            AssertDiscoveredMethod(testFixtureInAssemblyToDiscoverTestsIn, expectedNameOfFirstTestMethod, TestType.XUnit);
+        }
+
+
     }
 }
