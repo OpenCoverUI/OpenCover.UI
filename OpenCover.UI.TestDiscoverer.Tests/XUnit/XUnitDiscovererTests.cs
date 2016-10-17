aa76e63ccd64c9f22bfdc8082674a31bd7f9ab8a
MITEK\mkoning <mkoning@miteksystems.com>
2016-10-17 09:08:15 +0200

    Fix the xunitdiscoverertests

--- OpenCover.UI.TestDiscoverer.Tests/XUnit/XUnitDiscovererTests.cs
+++ OpenCover.UI.TestDiscoverer.Tests/XUnit/XUnitDiscovererTests.cs
@@ -1,18 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using NUnit.Framework;
-using OpenCover.UI.TestDiscoverer.TestResources.Xunit;
-
-//using OpenCover.UI.TestDiscoverer.TestResources.Xunit;
+using OpenCover.UI.Model.Test;
+using OpenCover.UI.TestDiscoverer.TestResources.xUnit;
 
 namespace OpenCover.UI.TestDiscoverer.Tests.XUnit
 {
-    
-    public class XUnitDiscovererTests: DiscovererTestsBase
+    [Ignore]
+    [TestFixture]
+    class XUnitDiscovererTests : DiscovererTestsBase
     {
-        [TestCase(typeof(RegularFacts), "RegularTestMethod")]
+        [TestCase(typeof(RegularxUnitTestClass), "RegularTestMethod")]
         public void Discover_Finds_Regular_Test_Fixture_And_Method(Type testFixtureInAssemblyToDiscoverTestsIn, string expectedNameOfFirstTestMethod)
         {
-            AssertDiscoveredMethod(testFixtureInAssemblyToDiscoverTestsIn, expectedNameOfFirstTestMethod);
+            AssertDiscoveredMethod(testFixtureInAssemblyToDiscoverTestsIn, expectedNameOfFirstTestMethod, TestType.XUnit);
         }
+
+        [TestCase(typeof(RegularxUnitTestClass.SubTestClass), "RegularSubTestClassMethod")]
+        [TestCase(typeof(RegularxUnitTestClass.SubTestClass.Sub2NdTestClass), "RegularSub2NdTestClassMethod")]
+        public void Discover_Finds_Sub_Test_Fixtures_And_Methods(Type testFixtureInAssemblyToDiscoverTestsIn, string expectedNameOfFirstTestMethod)
+        {
+            AssertDiscoveredMethod(testFixtureInAssemblyToDiscoverTestsIn, expectedNameOfFirstTestMethod, TestType.XUnit);
+        }
+
     }
 }
+
