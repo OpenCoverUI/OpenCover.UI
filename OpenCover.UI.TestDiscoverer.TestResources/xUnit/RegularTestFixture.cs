47e1177c70cea48785efe798e9aae58147166933
MITEK\mkoning <mkoning@miteksystems.com>
2016-10-17 09:04:39 +0200

    rename action

---
+++ OpenCover.UI.TestDiscoverer.TestResources/xUnit/RegularTestFixture.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OpenCover.UI.TestDiscoverer.TestResources.xUnit
+{
+    public class RegularxUnitTestClass
+    {
+
+        [Fact]
+        public void RegularTestMethod()
+        {
+
+        }
+
+        public class SubTestClass
+        {
+            [Fact]
+            public void RegularSubTestClassMethod()
+            {
+            }
+
+            public class Sub2NdTestClass
+            {
+                [Fact]
+                public void RegularSub2NdTestClassMethod()
+                {
+                }
+            }
+
+
+        }
+    }
+}
+
+
